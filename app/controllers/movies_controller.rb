ebec604d296d3dfdca015818471062b8a23dfbae
Seungri Kim <srkim@berkeley.edu>
2020-10-26 04:59:26 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,29 +12,10 @@ class MoviesController < ApplicationController
     if params[:ratings]
       @ratings_to_show = params[:ratings].keys
       #session[:filtered_rating] = @ratings_to_show
-    """elsif session[:filtered_rating]
-      query = {}
-      session[:filtered_rating].each do |rating|
-        query['ratings['+ rating + ']'] = 1
-      end
-      query['sort'] = params[:sort] if params[:sort]
-      session[:filtered_rating] = nil
-      flash.keep
-      redirect_to movies_path(query)"""
     else
       @ratings_to_show = @all_ratings
     end
-
     @movies = Movie.with_ratings(@ratings_to_show)
-
-    case params[:sort]
-    when 'title'
-      @movies.order!('title asc')
-      @title_class = "bg-warning hilite"
-    when 'release_date'
-      @movies.order!('release_date asc')
-      @release_date_class = "bg-warning hilite"
-    end
   end
   
   def new
